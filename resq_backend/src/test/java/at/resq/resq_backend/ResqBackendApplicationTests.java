3972f1b5cfa5c41ffb27765a1344a07912056447
david <david.leitner@leitlab.com>
2026-03-21 15:41:19 +0100

    init push

---
+++ resq_backend/src/test/java/at/resq/resq_backend/ResqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.resq.resq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
